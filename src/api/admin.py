1e5571b2a9fdf2adfffdf6cca2971eda63336a6d
Daniel <danolox@gmail.com>
2025-09-26 16:43:09 +0000

    Checking render problem with a commit

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,6 +3,7 @@ from flask_admin import Admin
 from .models import db, User, Recipe
 from flask_admin.contrib.sqla import ModelView
 
+
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -14,7 +15,9 @@ def setup_admin(app):
         column_list = [
             'id', 'email', 'username', 'full_name', 'plain_psswrd', 'profile_url', 'is_active', 'created_at', 'cloudinary_url', 'cloudinary_img_id'
         ]
-        column_searchable_list = ['email', 'username', 'full_name']
+        column_searchable_list = [
+                  'email', 'username', 'full_name']
+        
         column_filters = ['is_active']
 
     admin.add_view(UserAdmin(User, db.session))
