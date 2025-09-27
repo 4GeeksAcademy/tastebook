7cf56f557669302ac9f2be0a11c19d655cca6c0f
Daniel <danolox@gmail.com>
2025-09-27 08:17:48 +0000

    Added a comment explaining Render configuration file change

--- render_build.sh
+++ render_build.sh
@@ -5,6 +5,8 @@ set -o errexit
 npm install
 npm run build
 
+# "pip install pipenv &&" fue añadido porque Render NO reconocía el comando "pipenv",
+# supongo que fue por alguna actualización del entorno de Render.
 pip install pipenv && pipenv install
 
 pipenv run upgrade
