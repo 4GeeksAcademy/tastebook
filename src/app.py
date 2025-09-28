12340566e5d74de929a3d963e7523bcd9e0720cc
Daniel <danolox@gmail.com>
2025-09-28 23:16:20 +0000

    fix: Update app.run to use dynamic debug mode based on environment

--- src/app.py
+++ src/app.py
@@ -102,4 +102,5 @@ def serve_any_other_file(path):
 # This only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    DEBUG_MODE = ENV == "development"
+    app.run(host='0.0.0.0', port=PORT, debug=DEBUG_MODE)
