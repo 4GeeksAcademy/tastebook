31759d518534c6c991be6c44a2a1cc5147f0aa72
danolox <danolox@gmail.com>
2025-10-04 16:20:45 +0000

    fix: allowed to run server visible to host vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        // host: true,  // <-- ALWAYS allow access from external hosts (0.0.0.0), applies for both "start and "dev"
         port: 3000
     },
     build: {
         outDir: 'dist'
     }
 })
+
+// If you want to only expose to host in only one command, change "package.json":
+//
+// "scripts": {
+//      "start": "vite",
+//      "dev":   "vite --host",
+//
+//      ... (other scripts)
