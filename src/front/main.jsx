6564d245abf220b2b34d4a79fd9a57fe5351f403
Daniel <danolox@gmail.com>
2025-10-13 20:45:04 +0000

    Fix typo in MissingBackendUrlNotice import and fixed path to `env-file.png`

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -19,7 +19,7 @@ import './shared/styles/scss-bootstrap-custom-theme.scss'; // COMPLETE Custom Bo
 import { RouterProvider }           from "react-router-dom";  // Import RouterProvider to use the router
 import { router }                   from "./routes";  // Import the router configuration
 import { StoreProvider }            from './shared/hooks/useGlobalReducer';  // Import the StoreProvider for global state management
-import { MisssingBackendUrlNotice } from './modules/miscellaneous/4geeks-defatult-files/MisssingBackendUrlNotice';
+import { MissingBackendUrlNotice } from './modules/miscellaneous/4geeks-defatult-files/MissingBackendUrlNotice';
 
 const Main = () => {
     
@@ -28,7 +28,7 @@ const Main = () => {
     
     if(! import.meta.env.VITE_BACKEND_URL ||  import.meta.env.VITE_BACKEND_URL == "") return (
         <React.StrictMode>
-              <MisssingBackendUrlNotice/ >
+              <MissingBackendUrlNotice />
         </React.StrictMode>
         );
     return (
